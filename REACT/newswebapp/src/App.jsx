1b776f26a4101187a37048218e598642e35b24b5
Dilesh Fernando <153763931+DileshF@users.noreply.github.com>
2025-03-01 15:33:56 +0530

    added a button to log in(test)

--- REACT/newswebapp/src/App.jsx
+++ REACT/newswebapp/src/App.jsx
@@ -2,9 +2,13 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Test from './test.jsx'
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
 function App() {
   return(
-    <Card/>
+    <>
+      <Card/>
+      <Button/>
+    </>
   );
 }
 
--- REACT/newswebapp/src/Card.jsx
+++ REACT/newswebapp/src/Card.jsx
@@ -1,11 +1,11 @@
-import newslogo from './assets/logo.jpg'
+import newslogo from './assets/logo.png'
 
 function Card(){
     return(
         <div class="card">
             <img class="imga" src={newslogo} alt="newsapplogo"></img>
-            <h2 className='titname'>This is our admin pannal</h2>
-            <p>Admin</p>
+            <h2 class="titname">This is our admin pannal</h2>
+            <p class="crdtxt">Admin</p>
         </div>
     );
 }
